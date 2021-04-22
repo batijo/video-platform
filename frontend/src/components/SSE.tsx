3ab76c3c88d9f0751370a3339b329cdcfc9d273d
Kornelijus Tvarijanavičius <kornelitvari@protonmail.com>
2021-04-22 22:12:24 +0300

    sse scuffed but work yes

--- frontend/src/components/SSE.tsx
+++ frontend/src/components/SSE.tsx
@@ -2,29 +2,32 @@ import React from 'react'
 import axios from 'axios'
 import { useAppSelector } from '../index'
 
-const useEventSource = (url: string) => {
-  const [data, updateData] = React.useState('')
+// const useEventSource = (url: string) => {
+//   const [data, updateData] = React.useState('')
 
-  React.useEffect(() => {
-    const source = new EventSource(url)
+//   React.useEffect(() => {
+//     const source = new EventSource(url)
 
-    source.onmessage = (event) => {
-      console.log(event.data)
-      updateData(event.data)
-    }
-  }, [])
+//     source.onmessage = (event) => {
+//       console.log(event.data)
+//       updateData(data + event.data)
+//     }
+//   }, [])
 
-  return data
-}
+//   return data
+// }
 
 const SSE = () => {
   const token = useAppSelector(state => state.auth.token)
-  const data = useEventSource(`${window.origin}/api/sse/dashboard/${token}`)
-  if (!data) {
-    return <div />;
-  }
+  const [data, updateData] = React.useState('')
 
-  console.log(data)
+  const dataRef = React.useRef('')
+  dataRef.current = data
+
+  React.useEffect(() => {
+    const source = new EventSource(`${window.origin}/api/sse/dashboard/${token}`)
+    source.onmessage = (event) => updateData(dataRef.current + event.data)
+  }, [])
 
   return (
     <div>{data}</div>
