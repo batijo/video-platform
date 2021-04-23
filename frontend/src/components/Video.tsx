5065f0b1f040ce4ee8aa2e38155be84c955eddc8
Kornelijus Tvarijanavičius <kornelitvari@protonmail.com>
2021-04-23 04:26:10 +0300

    video list should work maybe

--- frontend/src/components/Video.tsx
+++ frontend/src/components/Video.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import ReactPlayer from 'react-player'
 
 import Video from '../types/video'
+import video from '../store/video'
 
 export const VideoDetail = ({ video }: { video: Video }) => {
   return (
@@ -24,24 +25,28 @@ export const VideoDetail = ({ video }: { video: Video }) => {
   )
 }
 
-export const VideoList = ({ videos }: { videos: Video[] }) => (
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
-    {videos.map(v => (
-      <div className="bg-white rounded-md">
-        <Link to={`/video/${v.id}`}>
-          <div className="relative">
-            <div className="absolute antialiased font-bold text-white top-2 right-2 bg-gray-800 px-2 rounded-md opacity-80">+</div>
-            <div className="absolute text-white bottom-2 right-2 bg-gray-800 px-1 rounded-md opacity-80">00:00</div>
-            <img className="rounded-t-md" src={thumbnail} />
+export const VideoList = ({ videos }: { videos: Video[] }) => {
+  const fVideos: Video[] = [...videos].filter(v => v.state === 'transcoded')
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {fVideos.map(v => (
+        <div className="bg-white rounded-md" key={v.id}>
+          <Link to={`/video/${v.id}`}>
+            <div className="relative">
+              <div className="absolute antialiased font-bold text-white top-2 right-2 bg-gray-800 px-2 rounded-md opacity-80">+</div>
+              <div className="absolute text-white bottom-2 right-2 bg-gray-800 px-1 rounded-md opacity-80">00:00</div>
+              <img className="rounded-t-md" src={thumbnail} />
+            </div>
+          </Link>
+          <div className="p-4 flex flex-row justify-between">
+            <span className="font-bold text-xl" id="title">
+              <Link to={`/video/${v.id}`}>{v.fileName}</Link>
+            </span>
+            <span className="text-md" id="title">2021-07-01</span>
           </div>
-        </Link>
-        <div className="p-4 flex flex-row justify-between">
-          <span className="font-bold text-xl" id="title">
-            <Link to={`/video/${v.id}`}>{v.title}</Link>
-          </span>
-          <span className="text-md" id="title">2021-07-01</span>
         </div>
-      </div>
-    ))}
-  </div>
-)
+      ))}
+    </div>
+  )
+}
